764a69e345878c586e8d23d0d906580f8488d283
Nick Basis <nickbasis@outlook.com>
2017-09-04 10:30:28 +0100

    Update login.js

--- tests/login.js
+++ tests/login.js
@@ -3,8 +3,8 @@ exports.command = function() {
 		this
 			.url('https://staging.cmdapp.com/cms')
 			.pause(1000)
-			.setValue('input#ctl00_mainContent_LoginCtrl_UserName', 'admin')
-			.setValue('input#ctl00_mainContent_LoginCtrl_Password', 'pallmall91')
+			.setValue('input#ctl00_mainContent_LoginCtrl_UserName', '****')
+			.setValue('input#ctl00_mainContent_LoginCtrl_Password', '******')
 			.pause(1000)
 			.click("#ctl00_mainContent_LoginCtrl_Login");
 	});
